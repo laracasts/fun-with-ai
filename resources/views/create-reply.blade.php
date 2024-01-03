08736d7e8e493ecacd1f6a35c8bcf3c464a38fb6
Jeffrey Way <jeffrey@jeffrey-way.com>
2024-01-03 11:54:33 -0500

    Episode 6 Complete

---
+++ resources/views/create-reply.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en" class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reply Example</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="p-6 bg-slate-100 max-w-xl mx-auto">
+    <form method="POST" action="/replies">
+        @csrf
+
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+                <h2 class="text-base font-semibold leading-7 text-gray-900">Create Reply</h2>
+                <p class="mt-1 text-sm leading-6 text-gray-600">This comment will be displayed publicly so be careful what you share.</p>
+
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="col-span-full">
+                        <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
+                        <div class="mt-2">
+                            <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Publish</button>
+        </div>
+    </form>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,39 @@
 <?php
 
-use App\AI\Assistant;
 use Illuminate\Support\Facades\Route;
+use OpenAI\Laravel\Facades\OpenAI;
 
 Route::get('/', function () {
-    return view('image', [
-        'messages' => session('messages', [])
-    ]);
+    return view('create-reply');
 });
 
-Route::post('/image', function () {
+Route::post('/replies', function () {
     $attributes = request()->validate([
-        'description' => ['required', 'string', 'min:3']
+        'body' => ['required', 'string']
     ]);
 
-    $assistant = new Assistant(session('messages', []));
+    $response = OpenAI::chat()->create([
+        'model' => 'gpt-3.5-turbo-1106',
+        'messages' => [
+            ['role' => 'system', 'content' => 'You are a forum moderator who always responds using JSON.'],
+            [
+                'role' => 'user',
+                'content' => <<<EOT
+                    Please inspect the following text and determine if it is spam.
 
-    $assistant->visualize($attributes['description']);
+                    {$attributes['body']}
 
-    session(['messages' => $assistant->messages()]);
+                    Expected Response Example:
 
-    return redirect('/');
+                    {"is_spam": true|false}
+                    EOT
+            ]
+        ],
+        'response_format' => ['type' => 'json_object']
+    ])->choices[0]->message->content;
+
+    $response = json_decode($response);
+    
+    // Trigger failed validation, display a flash message, abort...
+    return $response->is_spam ? 'THIS IS SPAM!': 'VALID POST';
 });
